c643af4c2684821876114293e0ef3d8a223a1d9a
Joksa132 <nikolajoksimovic419@gmail.com>
2025-08-01 10:14:36 +0000

    add tech skills icons and helper variable

---
+++ src/lib/data.tsx
@@ -0,0 +1,26 @@
+import javascriptIcon from "../assets/icons/javascript.svg";
+import htmlIcon from "../assets/icons/html5.svg";
+import cssIcon from "../assets/icons/css3.svg";
+import typescriptIcon from "../assets/icons/typescript.svg";
+import reactIcon from "../assets/icons/react.svg";
+import nextjsIcon from "../assets/icons/nextjs.svg";
+import expoIcon from "../assets/icons/expo.svg";
+import electronIcon from "../assets/icons/electron.svg";
+import expressIcon from "../assets/icons/express.svg";
+import nodejsIcon from "../assets/icons/nodejs.svg";
+import viteIcon from "../assets/icons/vite.svg";
+
+export const techStack = [
+  { name: "JavaScript", logo: javascriptIcon, isCenter: true },
+  { name: "HTML", logo: htmlIcon, isCenter: false },
+  { name: "CSS", logo: cssIcon, isCenter: false },
+  { name: "TypeScript", logo: typescriptIcon, isCenter: false },
+  { name: "React", logo: reactIcon, isCenter: false },
+  { name: "React Native", logo: reactIcon, isCenter: false },
+  { name: "Next.js", logo: nextjsIcon, isCenter: false },
+  { name: "Expo", logo: expoIcon, isCenter: false },
+  { name: "Vite", logo: viteIcon, isCenter: false },
+  { name: "Electron", logo: electronIcon, isCenter: false },
+  { name: "Express", logo: expressIcon, isCenter: false },
+  { name: "Node.js", logo: nodejsIcon, isCenter: false },
+];
