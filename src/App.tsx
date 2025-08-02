6be140d72ff34add7d9663a00d7dd8563ffc2dd8
Joksa132 <nikolajoksimovic419@gmail.com>
2025-08-02 12:29:24 +0200

    add inter font

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-black text-white">
+    <div className="min-h-screen bg-black text-white font-sans">
       <ImageModal
         isOpen={!!modalImage}
         onClose={closeImageModal}
